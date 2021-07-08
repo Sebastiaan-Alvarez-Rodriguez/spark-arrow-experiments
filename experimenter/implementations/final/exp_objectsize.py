e950e2d1bfe6b5b2a34119ca28217f41d4ba3348
Sebastiaan Alvarez Rodriguez <sebastiaanalva@gmail.com>
2021-07-08 11:43:03 +0200

    Modified objectsize experiment to go over selectivies, modes

--- experimenter/implementations/final/exp_objectsize.py
+++ experimenter/implementations/final/exp_objectsize.py
@@ -60,31 +60,43 @@ class CephExperiment(ExperimentInterface):
         ''''Get experiment ExecutionInterfaces.
         Returns:
             `iterable(internal.experiment.ExecutionInterfaces)`, containing all different setups we want to experiment with.'''
-        data_query = 'SELECT * FROM table WHERE total_amount > 6', #90% row selectivity, 100% column selectivity
-
+        data_queries = [
+            'SELECT * FROM table WHERE total_amount > 69', #1% row selectivity, 100% column selectivity
+            'SELECT * FROM table WHERE total_amount > 27', #10% row selectivity, 100% column selectivity
+            'SELECT * FROM table WHERE total_amount > 17', #25% row selectivity, 100% column selectivity
+            'SELECT * FROM table WHERE total_amount > 11', #50% row selectivity, 100% column selectivity
+            'SELECT * FROM table WHERE total_amount > 8', #75% row selectivity, 100% column selectivity
+            'SELECT * FROM table WHERE total_amount > 6', #90% row selectivity, 100% column selectivity
+            'SELECT * FROM table', # 100% row selectivity, 100% column selectivity
+        ]
+        row_selectivities = [1, 10, 25, 50, 75, 90, 100]
+ 
         object_sizes = [4, 8, 16, 32, 64, 128]
-        copy_multiplier, link_multiplier = (32, 64) #Total data size (for 128MB objects): 256GB
+        
+        copy_multiplier, link_multiplier = (32, 16) #Total data size (for 128MB objects): 64GB
         timestamp = datetime.now().isoformat()
 
         configs = []
-        for stripe in object_sizes:
-            result_dirname = '{}'.format(stripe)
-            configbuilder = ExperimentConfigurationBuilder()
-            configbuilder.set('mode', '--arrow-only')
-            configbuilder.set('runs', 21)
-            configbuilder.set('batchsize', 1024)
-            configbuilder.set('spark_driver_memory', '60G')
-            configbuilder.set('spark_executor_memory', '60G')
-            configbuilder.set('node_config', get_node_configuration())
-            configbuilder.set('stripe', stripe)
-            configbuilder.set('copy_multiplier', copy_multiplier)
-            configbuilder.set('link_multiplier', link_multiplier*(128//stripe))
-            configbuilder.set('remote_result_dir', fs.join('~', 'results', 'exp_objectsize_90', str(timestamp), result_dirname))
-            configbuilder.set('result_dir', fs.join(loc.result_dir(), 'exp_objectsize_90', str(timestamp), result_dirname))
-            configbuilder.set('data_path', fs.join(loc.data_generation_dir(), 'jayjeet_{}mb.pq'.format(stripe)))
-            configbuilder.set('data_query', '"{}"'.format(data_query))
-            config = configbuilder.build()
-            configs.append(config)
+        for mode in ['--arrow-only', '--spark-only']
+        for selectivity, data_query in zip(row_selectivities, data_queries):
+            for stripe in object_sizes:
+                result_dirname = '{:03d}_{:03d}'.format(stripe, selectivity)
+                configbuilder = ExperimentConfigurationBuilder()
+                configbuilder.set('mode', mode)
+                configbuilder.set('runs', 6)
+                configbuilder.set('batchsize', 1024)
+                configbuilder.set('spark_driver_memory', '60G')
+                configbuilder.set('spark_executor_memory', '60G')
+                configbuilder.set('node_config', get_node_configuration())
+                configbuilder.set('stripe', stripe)
+                configbuilder.set('copy_multiplier', copy_multiplier)
+                configbuilder.set('link_multiplier', link_multiplier*(128//stripe))
+                configbuilder.set('remote_result_dir', fs.join('~', 'results', 'exp_objectsize_90', str(timestamp), result_dirname))
+                configbuilder.set('result_dir', fs.join(loc.result_dir(), 'exp_objectsize_90', str(timestamp), result_dirname))
+                configbuilder.set('data_path', fs.join(loc.data_generation_dir(), 'jayjeet_{}mb.pq'.format(stripe)))
+                configbuilder.set('data_query', '"{}"'.format(data_query))
+                config = configbuilder.build()
+                configs.append(config)
 
         for idx, config in enumerate(configs):
             executionInterface = ExecutionInterface(config)
